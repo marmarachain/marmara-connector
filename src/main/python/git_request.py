3c97470cc84a8897b26ec21aea6ebccbb39612ae
rumeysayilmaz <era.ylmaz@gmail.com>
2021-06-18 18:33:11 +0300

    [add] comments added for explaining the functions inside git_request.py

--- src/main/python/git_request.py
+++ src/main/python/git_request.py
@@ -2,11 +2,20 @@ import requests
 
 download_url = 'https://github.com/marmarachain/marmara/releases/download/'
 
+"""
+requests to get the latest releases' tag_name of marmarachain user's marmara repo
+"""
+
 
 def git_request_tag():
     response = requests.get("https://api.github.com/repos/marmarachain/marmara/releases/latest")
     tag_name = response.json()["tag_name"]
     return tag_name
+
+
+"""
+combines the marmara download_url with latest releases' tag name to form the url for linux zip file
+"""
 
 
 def latest_marmara_zip_url():
