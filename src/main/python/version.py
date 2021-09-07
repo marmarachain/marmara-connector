d911e5b8918a3c1f4b0ebb36cad5578445741eab
rumeysayilmaz <era.ylmaz@gmail.com>
2021-09-07 18:25:00 +0300

    [add] modify functions in version.py for multiple uses i.e. in version control of app and blockchain

--- src/main/python/version.py
+++ src/main/python/version.py
@@ -1,14 +1,15 @@
 import requests
 
-download_url = 'https://github.com/marmarachain/marmara/releases/download/'
-
+marmara_download_url = 'https://github.com/marmarachain/marmara/releases/download/'
+app_download_url = 'https://github.com/marmarachain/marmara-connector/releases/download/'
+marmara_api_url = "https://api.github.com/repos/marmarachain/marmara/releases/latest"
 """
-requests to get the latest releases' tag_name of marmarachain user's marmara repo
+requests to get the latest releases' tag_name of a git api url
 """
 
 
-def git_request_tag():
-    response = requests.get("https://api.github.com/repos/marmarachain/marmara/releases/latest")
+def git_request_tag(api_url):
+    response = requests.get(api_url)
     tag_name = response.json()["tag_name"]
     return tag_name
 
@@ -20,6 +21,6 @@ combines the marmara download_url with latest releases' tag name to form the url
 
 def latest_marmara_zip_url():
     tag_name = git_request_tag()
-    latest_release_url = download_url + tag_name  # should be added + '/MCL-linux.zip' or '/MCL-win.zip according to OS
+    latest_release_url = marmara_download_url + tag_name  # should be added + '/MCL-linux.zip' or '/MCL-win.zip according to OS
     # print(linux_release_url)
     return latest_release_url
