692efa3780d80cd910d0eb99dd8b1f800657d843
rumeysayilmaz <era.ylmaz@gmail.com>
2021-09-08 14:30:29 +0300

    [ref] removed applicationcontext inheritance from the class, refactored code to get the icons from images resource bundle

--- src/main/python/qtguistyle.py
+++ src/main/python/qtguistyle.py
@@ -4,13 +4,16 @@ from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from qtguidesign import Ui_MainWindow
 from PyQt5.uic import loadUi
 
-class GuiStyle(ApplicationContext ,Ui_MainWindow):
+icon_path = ApplicationContext().get_resource("images")
+
+
+class GuiStyle(Ui_MainWindow):
 
     def __init__(self):
         # loadUi("qtguidesign.ui", self)  #  loadin from qtguidesign.ui
         self.setupUi(self)   # loading from qtguidesign.py
         # setting params
-        self.icon_path = self.get_resource("images")
+        self.icon_path = icon_path
         # stop button style
         self.stopchain_button.setIcon(QtGui.QIcon(self.icon_path + "/stop_icon.png"))
         self.stopchain_button.setIconSize(QtCore.QSize(32, 32))
