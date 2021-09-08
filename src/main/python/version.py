a9844e58f6e8f3e351f41a505f0f2163a575c5c1
rumeysayilmaz <era.ylmaz@gmail.com>
2021-09-08 18:31:10 +0300

    [add] added function to get the latest release of the app

--- src/main/python/version.py
+++ src/main/python/version.py
@@ -1,8 +1,9 @@
 import requests
 
 marmara_download_url = 'https://github.com/marmarachain/marmara/releases/download/'
-app_download_url = 'https://github.com/marmarachain/marmara-connector/releases/download/'
+app_release_url = 'https://github.com/marmarachain/marmara-connector/releases/'
 marmara_api_url = "https://api.github.com/repos/marmarachain/marmara/releases/latest"
+app_api_url = "https://api.github.com/repos/marmarachain/marmara-connector/releases/latest"
 """
 requests to get the latest releases' tag_name of a git api url
 """
@@ -19,8 +20,14 @@ combines the marmara download_url with latest releases' tag name to form the url
 """
 
 
-def latest_marmara_zip_url():
-    tag_name = git_request_tag()
-    latest_release_url = marmara_download_url + tag_name  # should be added + '/MCL-linux.zip' or '/MCL-win.zip according to OS
-    # print(linux_release_url)
+def latest_marmara_download_url():
+    tag_name = git_request_tag(marmara_api_url)
+    latest_download_url = marmara_download_url + tag_name
+    return latest_download_url
+
+
+def latest_app_release_url():
+    tag_name = git_request_tag(app_api_url)
+    latest_release_url = app_release_url + tag_name
     return latest_release_url
+
