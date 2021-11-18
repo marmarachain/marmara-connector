41c65c81341ca3b6a70fbb0c47ac6bc93a184829
rumeysayilmaz <era.ylmaz@gmail.com>
2021-11-18 18:28:35 +0300

    [add] added style key under user section for saving the selected style by user

--- src/main/python/configuration.py
+++ src/main/python/configuration.py
@@ -48,6 +48,7 @@ class ApplicationConfig:
                 self.config.add_section('PATHS')
                 self.config.add_section('USER')
                 self.config.set('USER', 'lang', 'EN')
+                self.config.set('USER', 'style', 'light')
                 self.config.write(configfile)
 
     def set_key_value(self, section, key, value):
